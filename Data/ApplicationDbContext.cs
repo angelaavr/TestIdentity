3ee1bb57f90b47ad88390a4405e033d4ee48d899
angelaavr <48030956+angelaavr@users.noreply.github.com>
2020-03-13 14:33:03 +0100

    First commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TestIdentity.Models;
+
+namespace TestIdentity.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+
+        public DbSet<Project> Projects { get; set; }
+
+    }
+}
